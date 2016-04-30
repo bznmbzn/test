28adb80501f439505789017d2fb24a5597cf0b24
mehmet bozan <mehmetbozan44@gmail.com>
2016-04-30 13:10:03 +0300

    Create random_isim.php

---
+++ random_isim.php
@@ -0,0 +1,16 @@
+<?php 
+header("Content-type:image/png");
+$resim=imagecreatetruecolor(500,500);
+$beyaz=imagecolorallocate($resim,200,255,170);
+$mavi=imagecolorallocate($resim,0,0,255);
+for($i=1;$i<=50;$i++)
+{
+imagestring($resim,rand(1,5),rand(0,425),rand(0,425),"Mehmet      MERVE ",
+imagecolorallocate($resim,rand(0,255),rand(0,255),rand(0,255)));
+}
+imagefill($resim,0,0,$beyaz);
+imagepng($resim);
+imagedestroy($resim);
+
+
+?>
