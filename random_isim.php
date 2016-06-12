043cfe3fffcb271fd24425ac831d454798c25832
mehmet bozan <mehmetbozan44@gmail.com>
2016-06-12 13:28:44 +0300

    Update random_isim.php

--- random_isim.php
+++ random_isim.php
@@ -5,7 +5,7 @@ $beyaz=imagecolorallocate($resim,200,255,170);
 $mavi=imagecolorallocate($resim,0,0,255);
 for($i=1;$i<=50;$i++)
 {
-imagestring($resim,rand(1,5),rand(0,425),rand(0,425),"Mehmet      MERVE ",
+imagestring($resim,rand(1,5),rand(0,425),rand(0,425),"Mehmet      BOZAN ",
 imagecolorallocate($resim,rand(0,255),rand(0,255),rand(0,255)));
 }
 imagefill($resim,0,0,$beyaz);
